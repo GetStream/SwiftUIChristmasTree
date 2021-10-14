b430145ca506ba1622b5ddeb9acbe277185dd821
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2021-10-14 12:13:43 +0300

    Initial Commit

---
+++ StreamiOSChatSDKPrototyping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StreamiOSChatSDKPrototyping
+//
+//  Created by amos.gyamfi@getstream.io on 14.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StreamiOSChatSDKPrototyping/StreamiOSChatSDKPrototypingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamiOSChatSDKPrototypingApp.swift
+//  StreamiOSChatSDKPrototyping
+//
+//  Created by amos.gyamfi@getstream.io on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct StreamiOSChatSDKPrototypingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
